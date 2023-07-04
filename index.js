f2c6e43bd38e02ccbd9874490d50937e34feec0d
vazcon <zaidniaazi@gmail.com>
2023-07-04 08:14:41 +0530

    "Add validation to restrict due date to current date or earlier"

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-alert("Check Date")
+// DOM 
 
 const amount = document.querySelector('#amount');
 const todayInput = document.querySelector('#today');
@@ -15,15 +15,60 @@ const dueDate = document.querySelector('#dueDate');
 
 let daysVal, primaryLateFee, secondaryFee, finalResults;
 
-window.onload = function(){
-  let today = new Date();
-  let dd = String(today.getDate()).padStart(2, '0');
-  let mm = String(today.getMonth() + 1).padStart(2, '0');
-  let yyyy = today.getFullYear();
+//date
 
-  today = yyyy + '-' + mm + '-' + dd;
-  document.getElementById("today").value = today;
+let todaysDate = document.getElementById("today");
+let dateWarning = document.getElementById("dateWarning");
+
+window.onload = function(){
+  fetch('http://worldtimeapi.org/api/timezone/Asia/Kolkata')
+  .then(response => response.json())
+  .then(data => {
+      let today = new Date(data.datetime);
+      let dd = String(today.getDate()).padStart(2, '0');
+      let mm = String(today.getMonth() + 1).padStart(2, '0'); 
+      let yyyy = today.getFullYear();
+      let formattedDate = yyyy + '-' + mm + '-' + dd;
+      todaysDate.value = formattedDate;
+      dueDate.max = formattedDate;
+  })
+  .catch((error) => {
+      console.error('Error:', error);
+  });
+
+  todaysDate.addEventListener("change", function() {
+      let todayVal = new Date(todaysDate.value);
+      let currentDateTime = new Date();
+      
+      if (todayVal.getTime() > currentDateTime.getTime()) {
+          dateWarning.textContent = "Future Date Selected";
+          dateWarning.style.color = 'red';
+      } else if (todayVal.getTime() < currentDateTime.getTime()) {
+          dateWarning.textContent = "Past Date Selected";
+          dateWarning.style.color = 'black';
+      } else {
+          dateWarning.textContent = '';
+      }
+      
+      dueDate.max = todaysDate.value; 
+  });
+
+
+  dueDate.addEventListener("change", function() {
+      let dueDateVal = new Date(dueDate.value);
+      let todayVal = new Date(todaysDate.value);
+
+      if (dueDateVal.getTime() > todayVal.getTime()) {
+          dueDate.style.borderColor = 'red';  
+          dateWarning.textContent = "Future date?"; 
+          dateWarning.style.color = 'red';
+      } else {
+          dueDate.style.borderColor = 'black';  
+          dateWarning.textContent = '';
+      }
+  });
 }
+
 
 function clearFields(){
   amount.value = '';
@@ -36,6 +81,9 @@ function clearFields(){
   printBtn.style.display = 'none';
 }
 
+
+// logic
+
 function calc(event){
   if(amount.value !== '' && dueDate.value !== '') {
     let amt = Number(amount.value);
@@ -45,7 +93,7 @@ function calc(event){
     let timeDiff = todayVal - dueDateVal;
     daysVal = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
 
-    primaryLateFee = (amt * 12 / 100) * (daysVal / 365);
+    primaryLateFee = (amt * 12 / 100) * (daysVal / 365);   // core logic
     secondaryFee = primaryLateFee * 18 / 100;
     finalResults = primaryLateFee + secondaryFee;
 
@@ -53,13 +101,13 @@ function calc(event){
     GST.textContent = `GST :${secondaryFee.toFixed(2)}`;
     totalResult.textContent = `Total: ${Math.round(finalResults)} (${finalResults.toFixed(2)})`;
     printBtn.style.display = 'block';
-    invoiceNumber.style.display = 'block'; // Show the invoice number field
-    invoiceNumber.style.animation = 'fadeInDown 0.5s ease'; // Apply fade-in animation
+    invoiceNumber.style.display = 'block'; 
+    invoiceNumber.style.animation = 'fadeInDown 0.5s ease'; 
   } else if(dueDate.value === '' || amount.value === '') {
     lateFee.textContent = 'N/A';
     GST.textContent = 'N/A';
     totalResult.textContent = 'N/A';
-    invoiceNumber.style.display = 'none'; // Hide the invoice number field
+    invoiceNumber.style.display = 'none'; 
     printBtn.style.display = 'none';
   }
 }
@@ -107,6 +155,8 @@ window.addEventListener('keydown', function(e) {
     }
   }
 }, true);
+
+//print button
 
 printBtn.addEventListener('click', function() {
   let printWindow = window.open('', '_blank');
