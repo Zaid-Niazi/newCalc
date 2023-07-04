ce397f531a2b5482f95e0e5659bbdba4e2b2a9ca
vazcon <zaidniaazi@gmail.com>
2023-07-04 07:13:07 +0530

    Add Print functionality

--- index.js
+++ index.js
@@ -1,33 +1,80 @@
+alert("Check Date")
+
 const amount = document.querySelector('#amount');
-const days = document.querySelector('#days');
-const result = document.querySelector('#result');
+const todayInput = document.querySelector('#today');
+const totalResult = document.querySelector('#totalResult');
+const lateFee = document.querySelector('#lateFee');
+const GST = document.querySelector('#GST');
 const clearBtn = document.querySelector('#clear');
+const printBtn = document.querySelector('#printBtn');
 const toggleBtn = document.querySelector('#toggleBtn');
 const formula = document.querySelector('#formula');
 const arrow = document.querySelector('#arrow');
 const toggleClearSwitch = document.querySelector('#toggleClear');
+const dueDate = document.querySelector('#dueDate');
+
+let daysVal, primaryLateFee, secondaryFee, finalResults;
+
+window.onload = function(){
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0');
+  let yyyy = today.getFullYear();
+
+  today = yyyy + '-' + mm + '-' + dd;
+  document.getElementById("today").value = today;
+}
 
 function clearFields(){
   amount.value = '';
-  days.value = '';
-  result.textContent = 'Result';
+  dueDate.value = '';
+  totalResult.textContent = 'Result';
+  lateFee.textContent = 'Fee on delay';
+  GST.textContent = 'GST on late fee';
+  invoiceNumber.value = ''; 
+  invoiceNumber.style.display = 'none'; 
+  printBtn.style.display = 'none';
 }
 
 function calc(event){
-  if(amount.value !== '' && days.value !== '') {
+  if(amount.value !== '' && dueDate.value !== '') {
     let amt = Number(amount.value);
-    let daysVal = Number(days.value);
-    let midResults = (amt*12/100)*(daysVal/365);
-    let finalResults = midResults + (midResults*18/100);
-    result.textContent = `${Math.round(finalResults)} (${finalResults.toFixed(2)})`;
-  } else if(days.value === '' || amount.value === '') {
-    result.textContent = 'Result';
+
+    let dueDateVal = new Date(dueDate.value);
+    let todayVal = new Date(document.getElementById("today").value);
+    let timeDiff = todayVal - dueDateVal;
+    daysVal = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+
+    primaryLateFee = (amt * 12 / 100) * (daysVal / 365);
+    secondaryFee = primaryLateFee * 18 / 100;
+    finalResults = primaryLateFee + secondaryFee;
+
+    lateFee.textContent = `Late : ${primaryLateFee.toFixed(2)}`;
+    GST.textContent = `GST :${secondaryFee.toFixed(2)}`;
+    totalResult.textContent = `Total: ${Math.round(finalResults)} (${finalResults.toFixed(2)})`;
+    printBtn.style.display = 'block';
+    invoiceNumber.style.display = 'block'; // Show the invoice number field
+    invoiceNumber.style.animation = 'fadeInDown 0.5s ease'; // Apply fade-in animation
+  } else if(dueDate.value === '' || amount.value === '') {
+    lateFee.textContent = 'N/A';
+    GST.textContent = 'N/A';
+    totalResult.textContent = 'N/A';
+    invoiceNumber.style.display = 'none'; // Hide the invoice number field
+    printBtn.style.display = 'none';
   }
 }
 
+document.getElementById("today").style.animation = "flashing 2s infinite";
+
+todayInput.addEventListener('input', function() {
+    this.style.animation = "none";
+    calc();
+});
+
+dueDate.addEventListener('input', calc);
+todayInput.addEventListener('input', calc);
 clearBtn.addEventListener('click', clearFields);
 amount.addEventListener('input', calc);
-days.addEventListener('input', calc);
 
 toggleClearSwitch.addEventListener('change', () => {
   if(toggleClearSwitch.checked) {
@@ -53,7 +100,25 @@ toggleBtn.addEventListener('click', () => {
 });
 
 window.addEventListener('keydown', function(e) {
-  if (e.key === 'Delete') {
-    clearFields();
+  if (e.keyIdentifier == 'U+000A' || e.keyIdentifier == 'Enter' || e.keyCode == 13) {
+    if (e.target.nodeName == 'INPUT' && e.target.type == 'text') {
+      e.preventDefault();
+      return false;
+    }
   }
+}, true);
+
+printBtn.addEventListener('click', function() {
+  let printWindow = window.open('', '_blank');
+  printWindow.document.write('<html><head><title>Print</title>');
+  printWindow.document.write('</head><body>');
+  printWindow.document.write('<h1>Merchant Invoice Delay Interest Calculation</h1>');
+  printWindow.document.write(`<p>Invoice Number: ${invoiceNumber.value}</p>`);
+  printWindow.document.write(`<p>Days Delayed: ${daysVal}</p>`);
+  printWindow.document.write(`<p>Fee on Delay: ${primaryLateFee.toFixed(2)}</p>`);
+  printWindow.document.write(`<p>GST on Late Fee: ${secondaryFee.toFixed(2)}</p>`);
+  printWindow.document.write(`<p>Total: ${Math.round(finalResults)} (${finalResults.toFixed(2)})</p>`);
+  printWindow.document.write('</body></html>');
+  printWindow.document.close();
+  printWindow.print();
 });
